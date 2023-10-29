795c7889f0d43d1ca0863a85c8a89b1f82185204
Nadav Timor <nadav0012@gmail.com>
2023-10-29 18:50:00 -0400

    sketching

--- main.py
+++ main.py
@@ -86,6 +86,7 @@ def speculative_sampling(x, draft_model, target_model, N, K):
 
     return x
 
+
 # With GPT2 PyTorch
 # def speculative_sampling_on_multiple_gpus(x: Tensor, draft_model, target_model, N, K, multi_gpu: bool=False):
 #     # NOTE: paper indexes arrays starting from 1, python indexes from 0, so
@@ -139,12 +140,17 @@ def speculative_sampling(x, draft_model, target_model, N, K):
 
 #     return x
 
-def speculative_sampling_on_multiple_gpus(x: Tensor, draft_model, target_model, N, K, multi_gpu: bool=False):
+
+def speculative_sampling_on_multiple_gpus(
+    target_model,
+    draft_model,
+    target_inputs: BatchEncoding,
+    draft_inputs: BatchEncoding,
+    N: int,
+    K: int,
+):
     # NOTE: paper indexes arrays starting from 1, python indexes from 0, so
     # we have to add an extra -1 term when indexing using n, T, or t
-    if multi_gpu:
-        draft_model.to(device="cuda:0", non_blocking=True)
-        target_model.to(device="cuda:1", non_blocking=True)
     n = len(x)
     T = len(x) + N
 
@@ -152,18 +158,22 @@ def speculative_sampling_on_multiple_gpus(x: Tensor, draft_model, target_model, 
         while n < T:
             prev_n = n
 
+            
             # Step 1: auto-regressive decode K tokens from draft model and get final p
-            x_draft = x
-            for _ in range(K):
-                p = draft_model(x_draft)
-                x_draft = np.append(x_draft, sample(p[-1]))
-            if multi_gpu:
-                x_draft.to(device="cpu", non_blocking=True)
+            # x_draft = x
+            # for _ in range(K):
+            #     p = draft_model(x_draft)
+            #     x_draft = np.append(x_draft, sample(p[-1]))
+            # if multi_gpu:
+            #     x_draft.to(device="cpu", non_blocking=True)
+            # Tensor.shape = (n_seq + K,)
+            x_draft: Tensor = draft_model.generate(**draft_inputs, max_new_tokens=K, do_sample=False).sequences.squeeze().to(device="cuda:1", non_blocking=True)
 
             # Step 2: target model forward passes on x_draft
-            q = target_model(x_draft)
-            if multi_gpu:
-                q.to(device="cpu", non_blocking=True)
+            # q = target_model(x_draft)
+            # if multi_gpu:
+            #     q.to(device="cpu", non_blocking=True)
+
 
             # Step 3: append draft tokens based on rejection criterion and resample
             # a token on rejection
@@ -216,7 +226,7 @@ def main(
     K: int = 4,
     temperature: float = 0.0,
     seed: int = 123,
-    multi_gpu: bool = False
+    multi_gpu: bool = False,
 ):
     # seed numpy rng
     np.random.seed(seed)
@@ -235,13 +245,6 @@ def main(
     input_ids: list = encoder.encode(prompt)
 
     def run_sampling_fn(decode_fn, input_ids, **kwargs):
-        start = time.perf_counter()
-        output_ids = decode_fn(x=input_ids, **kwargs)
-        text = encoder.decode(output_ids)
-        elapsed_time = time.perf_counter() - start
-        return text, elapsed_time
-    
-    def run_sampling_fn_via_transformers(model, inputs: BatchEncoding, max_new_tokens: int):
         start = time.perf_counter()
         output_ids = decode_fn(x=input_ids, **kwargs)
         text = encoder.decode(output_ids)
@@ -267,17 +270,36 @@ def main(
     )
 
     if multi_gpu:
-        tokenizer = AutoTokenizer.from_pretrained("gpt2")
-        inputs: BatchEncoding = tokenizer(prompt, return_tensors="pt")
-        model = GPT2LMHeadModel.from_pretrained("gpt2")
-        speculative_multi_gpu_text, speculative_multi_gpu_time = run_sampling_fn(
-            decode_fn=speculative_sampling_on_multiple_gpus,
-            input_ids=torch.tensor(input_ids),
+        target_model_id: str = "gpt2"
+        draft_model_id: str = "distilgpt2"
+        
+        target_tokenizer = AutoTokenizer.from_pretrained(target_model_id)
+        draft_tokenizer = AutoTokenizer.from_pretrained(draft_model_id)
+        
+        target_inputs = target_tokenizer("Hello, my dog is cute", return_tensors="pt").to(device="cuda:1", non_blocking=True)
+        draft_inputs = draft_tokenizer("Hello, my dog is cute", return_tensors="pt").to(device="cuda:0", non_blocking=True)
+        
+        assert torch.all(draft_inputs.input_ids.cpu() == target_inputs.input_ids.cpu())
+        assert torch.all(draft_inputs.attention_mask.cpu() == target_inputs.attention_mask.cpu())
+
+        target_model = GPT2LMHeadModel.from_pretrained(target_model_id).to(device="cuda:1", non_blocking=True)
+        # # Set pad_token_id to eos_token_id because GPT2 does not have a PAD token, following
+        # # https://github.com/huggingface/transformers/blob/722e9364916e527e8d46cbd828a1516bf6aaebd6/src/transformers/generation/utils.py#L4341C10-L4341C10
+        # if target_model.config.pad_token_id is None:
+        #     target_model.config.pad_token_id = target_model.config.eos_token_id
+        draft_model = GPT2LMHeadModel.from_pretrained(draft_model_id).to(device="cuda:0", non_blocking=True)
+
+        start = time.perf_counter()
+        outputs = speculative_sampling_on_multiple_gpus(
             target_model=target_model,
             draft_model=draft_model,
+            target_inputs=target_inputs,
+            draft_inputs=draft_inputs,
             N=n_tokens_to_generate,
             K=K,
         )
+        elapsed_time = time.perf_counter() - start
+        text: str = target_tokenizer.batch_decode(outputs)
 
     # print results
     print()
@@ -293,8 +315,8 @@ def main(
     if multi_gpu:
         print("Speculative Decode on Multiple GPUs")
         print("------------------")
-        print(f"Time = {speculative_multi_gpu_time:.2f}s")
-        print(f"Text = {speculative_multi_gpu_text}")
+        print(f"Time = {elapsed_time:.2f}s")
+        print(f"Text = {text}")
 
 
 if __name__ == "__main__":
