ef7f58ab44297c32ff0f8a328f0b8dc3649912d9
Nadav Timor <nadav0012@gmail.com>
2023-11-11 08:00:19 -0500

    Reformatting

--- horace_he.py
+++ horace_he.py
@@ -3,14 +3,33 @@
 import torch
 
 
-def speculative_decode(model: LLaMA, draft_model: LLaMA, cur_token: torch.Tensor, input_pos: int, speculate_k: int, **sampling_kwargs) -> torch.Tensor:
+def speculative_decode(
+    model: LLaMA,
+    draft_model: LLaMA,
+    cur_token: torch.Tensor,
+    input_pos: int,
+    speculate_k: int,
+    **sampling_kwargs
+) -> torch.Tensor:
     # draft model inference sequentially
     device = cur_token.device
-    orig_input_pos = torch.tensor([input_pos], dtype=torch.int64, device=cur_token.device)
-    draft_tokens, draft_probs = decode_n_tokens(draft_model, cur_token.view(1, -1), orig_input_pos.clone(), speculate_k, **sampling_kwargs)
+    orig_input_pos = torch.tensor(
+        [input_pos], dtype=torch.int64, device=cur_token.device
+    )
+    draft_tokens, draft_probs = decode_n_tokens(
+        draft_model,
+        cur_token.view(1, -1),
+        orig_input_pos.clone(),
+        speculate_k,
+        **sampling_kwargs
+    )
     draft_tokens = torch.cat(draft_tokens)
     # parallel inference on target model using draft tokens
-    target_logits = model_forward(model, torch.cat([cur_token.view(1), draft_tokens]).view(1, -1), torch.arange(input_pos, input_pos + speculate_k + 1, device=cur_token.device))
+    target_logits = model_forward(
+        model,
+        torch.cat([cur_token.view(1), draft_tokens]).view(1, -1),
+        torch.arange(input_pos, input_pos + speculate_k + 1, device=cur_token.device),
+    )
     target_probs = logits_to_probs(target_logits[0], **sampling_kwargs)
     draft_probs = torch.stack(draft_probs)
     # q: target prob, p: draft prob
@@ -18,10 +37,11 @@ def speculative_decode(model: LLaMA, draft_model: LLaMA, cur_token: torch.Tensor
     # q < p: q/p prob to accept draft token
     p = draft_probs[torch.arange(0, speculate_k, device=device), draft_tokens]
     q = target_probs[torch.arange(0, speculate_k, device=device), draft_tokens]
-    accept_draft_prob = torch.minimum(torch.ones(()), q[:speculate_k]/ p)
-    rejected_locations = (torch.rand_like(accept_draft_prob) >
-    accept_draft_prob).nonzero()
-    if rejected_locations.shape[0] == 0: # All draft tokens have been accepted
+    accept_draft_prob = torch.minimum(torch.ones(()), q[:speculate_k] / p)
+    rejected_locations = (
+        torch.rand_like(accept_draft_prob) > accept_draft_prob
+    ).nonzero()
+    if rejected_locations.shape[0] == 0:  # All draft tokens have been accepted
         accept_length = speculate_k + 1
         last_token = multinomial_sample_one_no_sync(target_probs[-1])
         # fill last token into draft model
@@ -31,12 +51,11 @@ def speculative_decode(model: LLaMA, draft_model: LLaMA, cur_token: torch.Tensor
             orig_input_pos + speculate_k,
         )
         return torch.cat([draft_tokens, last_token])
-    else:
-        accept_length = rejected_locations[0].item()
-        p = draft_probs[accept_length]
-        q = target_probs[accept_length]
-        new = q - p
-        new = torch.where(new > 0, new, 0.0)
-        new = new / new.sum()
-        next_token = multinomial_sample_one_no_sync(new)
-        return torch.cat([draft_tokens[:accept_length], next_token])
+    accept_length = rejected_locations[0].item()
+    p = draft_probs[accept_length]
+    q = target_probs[accept_length]
+    new = q - p
+    new = torch.where(new > 0, new, 0.0)
+    new = new / new.sum()
+    next_token = multinomial_sample_one_no_sync(new)
+    return torch.cat([draft_tokens[:accept_length], next_token])
